291b9a7fd021ff86a682bf7448dc6350b87250e3
James Beard <james@smallsaucepan.com>
2024-06-14 15:15:56 +1000

    Inlining a custom equal function, rather than using the heavier, more general purpose deep-equal library.

--- index.ts
+++ index.ts
@@ -10,8 +10,8 @@ import {
   MultiLineString,
   MultiPoint,
   MultiPolygon,
+  GeoJsonProperties,
 } from "geojson";
-import equal from "deep-equal";
 
 /**
 
@@ -210,6 +210,41 @@ function geojsonEquality(
 
   return eq.compare(g1, g2);
 }
+
+// Adapted from https://medium.com/syncfusion/5-different-ways-to-deep-compare-javascript-objects-6708a0da9f05
+function equal(object1: GeoJsonProperties, object2: GeoJsonProperties) {
+  if (object1 === null && object2 === null) {
+    return true;
+  }
+
+  if (object1 === null || object2 === null) {
+    return false;
+  }
+
+  const objKeys1 = Object.keys(object1);
+  const objKeys2 = Object.keys(object2);
+
+  if (objKeys1.length !== objKeys2.length) return false;
+
+  for (var key of objKeys1) {
+    const value1 = object1[key];
+    const value2 = object2[key];
+
+    const isObjects = isObject(value1) && isObject(value2);
+
+    if (
+      (isObjects && !equal(value1, value2)) ||
+      (!isObjects && value1 !== value2)
+    ) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const isObject = (object: any) => {
+  return object != null && typeof object === "object";
+};
 
 export { GeojsonEquality, geojsonEquality };
 export default GeojsonEquality;
